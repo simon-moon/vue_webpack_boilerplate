ba8d6364e87eea5cc0fbcf971bae80494264d6b4
simon-moon <simonmoon@live.com>
2016-03-21 14:16:32 -0700

    Added Vue-Router, modified applicaiton to accept routing, removed unneeded files

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,42 @@
 <template>
   <div id="app">
-    <img class="logo" src="./assets/logo.png">
-    <hello></hello>
+    <img class="logo" src="./assets/K_logo.png">
     <p>
-      Welcome to your Vue.js app. To get started, take a look at the
-      <a href="https://github.com/vuejs-templates/webpack#folder-structure" target="_blank">README</a>
-      of this template. If you have any issues with the setup, please file an issue at this template's repository.
-    </p>
+      <a href="/home">Home</a> - <a href="/test" >Test</a> 
+    </p> 
     <p>
-      For advanced configurations, checkout the docs for
-      <a href="http://webpack.github.io/" target="_blank">Webpack</a> and
-      <a href="http://vuejs.github.io/vue-loader/" target="_blank">vue-loader</a>.
+      Welcome to New Schweser - a Vue.js app. 
     </p>
-    <p>
-      You may also want to checkout
-      <a href="https://github.com/vuejs/vue-router/" target="_blank">vue-router</a> for routing and
-      <a href="https://github.com/vuejs/vuex/" target="_blank">vuex</a> for state management.
+    <p>  
+      This is the Parent view Located in src/App.vue
     </p>
+    <!---Main View -->
+    <router-view>
+    </router-view>
   </div>
 </template>
 
-<script>
-import Hello from './components/Hello'
-
-export default {
-  components: {
-    Hello
-  }
-}
-</script>
-
 <style>
-html {
-  height: 100%;
-}
+  html {
+    height: 100%;
+  }
 
-body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-}
+  body {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+  }
 
-#app {
-  margin-top: -100px;
-  max-width: 600px;
-  font-family: Helvetica, sans-serif;
-  text-align: center;
-}
+  #app {
+    margin-top: -100px;
+    max-width: 600px;
+    font-family: Helvetica, sans-serif;
+    text-align: center;
+  }
 
-.logo {
-  width: 100px;
-  height: 100px
-}
+  .logo {
+    width: 100px;
+    height: 100px;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,36 @@
 import Vue from 'vue'
 import App from './App'
+import VueRouter from 'vue-router'
 
 /* eslint-disable no-new */
 new Vue({
   el: 'body',
   components: { App }
 })
+
+Vue.use(VueRouter)
+
+var router = new VueRouter({
+  hashbang: false,
+  history: true
+})
+
+router.map({
+  '/': {
+    component: function (resolve) {
+      require(['./components/Home.vue'], resolve)
+    }
+  },
+  '/home': {
+    component: function (resolve) {
+      require(['./components/Home.vue'], resolve)
+    }
+  },
+  '/test': {
+    component: function (resolve) {
+      require(['./components/Test.vue'], resolve)
+    }
+  }
+})
+
+router.start(App, '#app')
